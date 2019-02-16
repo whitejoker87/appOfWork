6ededc4922915c56909c9a9ff817ab71b9e6a346
Kolesnikov Vyacheslav <pride.v.kolesnikov@hotmail.com>
2019-02-16 18:49:01 +0500

    backup

--- app/src/main/java/ru/jobni/jobni/SplashScreenActivity.kt
+++ app/src/main/java/ru/jobni/jobni/SplashScreenActivity.kt
@@ -33,6 +33,5 @@ class SplashScreenActivity : AppCompatActivity() {
             startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
             finish()
         }, duration)
-
     }
 }
