b6deb19638098b50c06a1297e63e2a2cb3bd9cb8
NetylkinOV <gpcore@mail.ru>
2019-02-23 17:12:38 +0700

    Обновление Retrofit Получение максимального кол-ва ответов от сервера для поиска

--- app/src/main/java/ru/jobni/jobni/utils/RetrofitQuery.kt
+++ app/src/main/java/ru/jobni/jobni/utils/RetrofitQuery.kt
@@ -6,5 +6,5 @@ import retrofit2.http.Query
 
 interface RetrofitQuery {
     @GET("api/search_competence_by_vacancy/")
-    fun loadCompetence(@Query("name") competenceName: String): Call<List<String>>
+    fun loadCompetence(@Query("name") competenceName: String, @Query("count") count: Int): Call<List<String>>
 }
