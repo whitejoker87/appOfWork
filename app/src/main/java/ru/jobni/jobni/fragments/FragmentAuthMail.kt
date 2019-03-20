f8f91d8dcb2eb491bc427f3e98da4a8d4b2d6164
NetylkinOV <gpcore@mail.ru>
2019-03-20 19:17:44 +0700

    v10

--- app/src/main/java/ru/jobni/jobni/fragments/FragmentAuthMail.kt
+++ app/src/main/java/ru/jobni/jobni/fragments/FragmentAuthMail.kt
@@ -1,9 +1,11 @@
 package ru.jobni.jobni.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -36,6 +38,7 @@ class FragmentAuthMail : Fragment() {
                 "Auth" -> setFragment(FragmentAuth())
                 else -> setFragment(FragmentAuth())
             }
+            closeKeyboard()
         })
 
         return view
@@ -46,5 +49,14 @@ class FragmentAuthMail : Fragment() {
                 .replace(R.id.fragment_container, fragment)
                 .addToBackStack(null)
                 .commit()
+    }
+
+    private fun closeKeyboard() {
+        //скрываем клавиатуру
+        val viewCloseButton = activity?.currentFocus
+        viewCloseButton?.let { v ->
+            val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(v.windowToken, 0)
+        }
     }
 }
