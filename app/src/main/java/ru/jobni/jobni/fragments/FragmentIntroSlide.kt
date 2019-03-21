52c4aaa92573c91790bf953cf415e4a6121b7669
NetylkinOV <gpcore@mail.ru>
2019-03-21 23:29:54 +0700

    Правка слайдера при старте приложения

--- app/src/main/java/ru/jobni/jobni/fragments/FragmentIntroSlide.kt
+++ app/src/main/java/ru/jobni/jobni/fragments/FragmentIntroSlide.kt
@@ -1,19 +1,15 @@
 package ru.jobni.jobni.fragments
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
-import androidx.lifecycle.get
 import ru.jobni.jobni.R
-import ru.jobni.jobni.databinding.ActivityMainBinding
-import ru.jobni.jobni.viewmodel.MainViewModel
 import ru.jobni.jobni.databinding.CIntro05Binding
+import ru.jobni.jobni.viewmodel.MainViewModel
 
 class FragmentIntroSlide : Fragment() {
 
@@ -21,6 +17,20 @@ class FragmentIntroSlide : Fragment() {
     private lateinit var binding: CIntro05Binding
 
     private var param1: Int = 0
+
+    companion object {
+
+        private const val ARG_PARAM1 = "param1"
+
+        @JvmStatic
+        fun newInstance(num: Int) =
+                FragmentIntroSlide().apply {
+                    arguments = Bundle().apply {
+                        putInt(ARG_PARAM1, num)
+                    }
+                }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,16 +40,15 @@ class FragmentIntroSlide : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
 
-        val view = inflater.inflate(param1, container, false);
-        if (param1 == R.layout.c_intro_05){
+        val view = inflater.inflate(param1, container, false)
+        return if (param1 == R.layout.c_intro_05) {
             binding = DataBindingUtil.inflate(inflater, param1, container, false)
-            return binding.root
-        }
-        else return view
+            binding.root
+        } else view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -48,17 +57,5 @@ class FragmentIntroSlide : Fragment() {
             viewModel = ViewModelProviders.of(activity!!).get(MainViewModel::class.java)
             binding.viewmodel = viewModel
         }
-    }
-
-    companion object {
-        @JvmStatic
-        fun newInstance(num: Int) =
-            FragmentIntroSlide().apply {
-                arguments = Bundle().apply {
-                    putInt(ARG_PARAM1, num)
-                }
-            }
-
-        private const val ARG_PARAM1 = "param1"
     }
 }
