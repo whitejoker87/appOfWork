6b054d31b89675be4546e866f0f1f365f22a8c9b
NetylkinOV <gpcore@mail.ru>
2019-03-27 15:57:02 +0700

    v4

--- app/src/main/java/ru/jobni/jobni/utils/BindingAdapters.kt
+++ app/src/main/java/ru/jobni/jobni/utils/BindingAdapters.kt
@@ -1,6 +1,7 @@
 package ru.jobni.jobni.utils
 
 
+import android.view.View
 import android.widget.AdapterView
 import android.widget.ListView
 import android.widget.SearchView
@@ -12,87 +13,108 @@ import androidx.databinding.BindingMethods
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import ru.jobni.jobni.viewmodel.MainViewModel
 
 
 @BindingMethods(
-    BindingMethod(
-        type = BindingAdapters::class,
-        attribute = "app:onNavigationItemSelected",
-        method = "setOnNavigationItemSelectedListener"
-    )
+        BindingMethod(
+                type = BindingAdapters::class,
+                attribute = "app:onNavigationItemSelected",
+                method = "setOnNavigationItemSelectedListener"
+        )
 )
 
 object BindingAdapters {
 
     //открываем дравлер(пока не работает)
     @BindingAdapter("app:openDrawer")
-    @JvmStatic fun openDrawer(drawerLayout: DrawerLayout, gravity: Int) {
+    @JvmStatic
+    fun openDrawer(drawerLayout: DrawerLayout, gravity: Int) {
         //if (gravity == 0) drawerLayout.closeDrawer(GravityCompat.END)
         drawerLayout.openDrawer(gravity)
     }
 
     //блокировка дравлеров
     @BindingAdapter("app:setDrawerLockMode")
-    @JvmStatic fun setDrawerLockMode(drawerLayout: DrawerLayout, mode: Int) {
+    @JvmStatic
+    fun setDrawerLockMode(drawerLayout: DrawerLayout, mode: Int) {
         drawerLayout.setDrawerLockMode(mode)
     }
 
     //слушаем нижнее меню(пока не работает)
     @BindingAdapter("app:onNavigationItemSelected")
-    @JvmStatic fun setOnNavigationItemSelectedListener(
-        view: BottomNavigationView, listener: BottomNavigationView.OnNavigationItemSelectedListener
+    @JvmStatic
+    fun setOnNavigationItemSelectedListener(
+            view: BottomNavigationView, listener: BottomNavigationView.OnNavigationItemSelectedListener
     ) {
         view.setOnNavigationItemSelectedListener(listener)
     }
 
     //слушаем ресайклвью карточек
     @BindingAdapter("app:onScrollListener")
-    @JvmStatic fun setOnScrollListener(view: RecyclerView, listener: RecyclerView.OnScrollListener) {
+    @JvmStatic
+    fun setOnScrollListener(view: RecyclerView, listener: RecyclerView.OnScrollListener) {
         view.addOnScrollListener(listener)
     }
 
     //слушаем строку поиска
     @BindingAdapter("app:onQueryTextListener")
-    @JvmStatic fun setQueryTextListener(view: SearchView, listener: SearchView.OnQueryTextListener) {
+    @JvmStatic
+    fun setQueryTextListener(view: SearchView, listener: SearchView.OnQueryTextListener) {
         view.setOnQueryTextListener(listener)
     }
 
     //изменение в строке поиска
     @BindingAdapter("app:query")
-    @JvmStatic fun query(view: SearchView, query: String) {
+    @JvmStatic
+    fun query(view: SearchView, query: String) {
         view.setQuery(query, true)
     }
 
     //обработка клика списка из поиска
     @BindingAdapter("app:onListViewItemClickListener")
-    @JvmStatic fun setOnItemClickListener(view: ListView, listener: AdapterView.OnItemClickListener) {
+    @JvmStatic
+    fun setOnItemClickListener(view: ListView, listener: AdapterView.OnItemClickListener) {
         view.onItemClickListener = listener
     }
 
     //для ресайклвью карточек
     @BindingAdapter("app:fixedSize")
-    @JvmStatic fun fixedSize(view: RecyclerView, fixSize: Boolean) {
+    @JvmStatic
+    fun fixedSize(view: RecyclerView, fixSize: Boolean) {
         view.setHasFixedSize(fixSize)
     }
 
     //преобразование для отображения з/п в карточках
     @BindingAdapter("app:formattedTextSalary")
-    @JvmStatic fun setFormattedText(view: TextView, text: String) {
-            val sb = StringBuffer(text)
-            when {
-                text.length < 3 -> {
-                    //do nothing
-                }
-                text.length == 4 -> sb.insert(1, " ")
-                text.length == 6 -> sb.insert(3, " ")
-                else -> sb.insert(2, " ")
+    @JvmStatic
+    fun setFormattedText(view: TextView, text: String) {
+        val sb = StringBuffer(text)
+        when {
+            text.length < 3 -> {
+                //do nothing
             }
-            view.text = sb.toString()
+            text.length == 4 -> sb.insert(1, " ")
+            text.length == 6 -> sb.insert(3, " ")
+            else -> sb.insert(2, " ")
+        }
+        view.text = sb.toString()
+    }
+
+    // Нажатие кнопки с закрытием в левом меню (авторизация/регистрация)
+    @BindingAdapter("app:clickNavMenuBtn", "app:authBtn")
+    @JvmStatic
+    fun onClickNavMenuBtn(view: View, clickNavMenuBtn: MainViewModel, authBtn: String) {
+        view.setOnClickListener {
+            clickNavMenuBtn.setFragmentLaunch(authBtn)
+            clickNavMenuBtn.setOpenDrawerLeft(false)
+        }
     }
 
     //преобразование для сисков в карточках
     @BindingConversion
-    @JvmStatic fun convertListToString(list: List<String>): String {
+    @JvmStatic
+    fun convertListToString(list: List<String>): String {
         val sb = StringBuilder()
         for (str in list) {
             if (sb.isNotEmpty()) sb.append(", ")
@@ -100,8 +122,6 @@ object BindingAdapters {
         }
         return sb.toString()
     }
-
-
 
 
 //    @BindingAdapter("app:onNavigationItemSelected")
