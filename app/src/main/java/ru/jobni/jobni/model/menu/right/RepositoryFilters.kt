00823619dde073d92dc5a35c19e020d816ba0095
white_joker <alexeybjspishe@mail.ru>
2019-03-28 00:52:38 +0300

    make right menu recycler

--- app/src/main/java/ru/jobni/jobni/model/menu/right/RepositoryFilters.kt
+++ app/src/main/java/ru/jobni/jobni/model/menu/right/RepositoryFilters.kt
@@ -1,6 +1,5 @@
 package ru.jobni.jobni.model.menu.right
 
-import com.google.gson.annotations.SerializedName
 import ru.jobni.jobni.R
 import ru.jobni.jobni.model.menu.NavigationChild
 import ru.jobni.jobni.model.menu.NavigationParent
@@ -13,126 +12,75 @@ import java.util.*
 object RepositoryFilters {
 
     fun makeNavigationListFilters(list: MutableList<Any>): List<NavigationParent> {
-        val headers = ArrayList<String>()
+        val headers = ArrayList<NavigationParent>()
         list.forEach { str: Any ->
-            if (str is String) headers.add(str)
-            else when (str) {
-                is Zarplata -> headers.add("Зарплата")
-                is Social_packet -> headers.add("Социальный пакет")
-                is Auto -> headers.add("Авто")
-                is Raiting -> headers.add("Рейтинг")
-            }
+            headers.add(makeParent(str))
         }
-        return Arrays.asList(
-                list.forEach { str: Any ->
-                    if (str is String) headers.add(str)
-                    else makeParentOne(str)
-
-
-
-
-
-//                        when (str) {
-//                        is Zarplata -> headers.add("Зарплата")
-//                        is Social_packet -> headers.add("Социальный пакет")
-//                        is Auto -> headers.add("Авто")
-//                        is Raiting -> headers.add("Рейтинг")
-//                    }
-                }
-//                makeParentOne(),
-//                makeParentTwo(),
-//                makeParentThree(),
-//                makeParentFour()
-//        )
+        return headers
     }
 
-    private fun makeParentOne(str: Any): NavigationParent {
+    private fun makeParent(str: Any): NavigationParent {
         return NavigationParent(
-                        when (str) {
-                        is Zarplata -> "Зарплата"
-                        is Social_packet -> "Социальный пакет"
-                        is Auto -> "Авто"
-                        is Raiting -> "Рейтинг"
-                            else -> "Опять что-то новое"
-                    },
-                makeParentOneChild(str),
+                when (str) {
+                    is String -> str
+                    is Zarplata -> "Зарплата"
+                    is Social_packet -> "Социальный пакет"
+                    is Auto -> "Авто"
+                    is Raiting -> "Рейтинг"
+                    else -> "Опять что-то новое"
+                },
+                makeChild(str),
                 R.drawable.ic_user
         )
     }
 
-    private fun makeParentOneChild(str: Any): List<NavigationChild> {
+    private fun makeChild(str: Any): List<NavigationChild> {
         when (str) {
+            is String -> return listOf()
             is Zarplata -> {
-                val(salary_level_newbie,
+                val (salary_level_newbie,
                         salary_level_experienced,
                         monthly_rate,
-                        rate_per_hour) = str as Zarplata
+                        rate_per_hour) = str
                 return listOf(NavigationChild(salary_level_newbie, 0),
                         NavigationChild(salary_level_experienced, 0),
                         NavigationChild(monthly_rate, 0),
                         NavigationChild(rate_per_hour, 0))
             }
-            is Social_packet -> "Социальный пакет"
-            is Auto -> "Авто"
-            is Raiting -> "Рейтинг"
-            else -> "Опять что-то новое"
+            is Social_packet -> {
+                val (housing,
+                        travel,
+                        meals,
+                        gym,
+                        childcare_facilities,
+                        training) = str
+                return listOf(NavigationChild(housing, 0),
+                        NavigationChild(travel, 0),
+                        NavigationChild(meals, 0),
+                        NavigationChild(gym, 0),
+                        NavigationChild(childcare_facilities, 0),
+                        NavigationChild(training, 0))
+            }
+            is Auto -> {
+                val (year_of_manufacture,
+                        fuel_consumption_city,
+                        fuel_consumption_highway,
+                        cargo_capacity,
+                        capacity_of_people) = str
+                return listOf(NavigationChild(year_of_manufacture, 0),
+                        NavigationChild(fuel_consumption_city, 0),
+                        NavigationChild(fuel_consumption_highway, 0),
+                        NavigationChild(cargo_capacity, 0),
+                        NavigationChild(capacity_of_people, 0))
+            }
+            is Raiting -> {
+                val (labor_rating,
+                        business_rating) = str
+                return listOf(NavigationChild(labor_rating, 0),
+                        NavigationChild(business_rating, 0))
+            }
+            else -> return listOf(NavigationChild("Опять что-то новое", 0))
         }
-        return listOf(val (competence,
-                languages,
-                work_places,
-                employment,
-                format_of_work,
-                field_of_activity,
-                age_company,
-                required_number_of_people,
-                zarplata, social_packet,
-                auto,
-                raiting) = str)
-    }
-
-    private fun makeParentTwo(): NavigationParent {
-        return NavigationParent(
-                "Отзывы",
-                makeParentTwoChild(),
-                R.drawable.ic_user
-        )
-    }
-
-    private fun makeParentTwoChild(): List<NavigationChild> {
-        return listOf()
-    }
-
-    private fun makeParentThree(): NavigationParent {
-        return NavigationParent(
-                "Профиль",
-                makeParentThreeChild(),
-                R.drawable.ic_user
-        )
-    }
-
-    private fun makeParentThreeChild(): List<NavigationChild> {
-        return listOf()
-    }
-
-    private fun makeParentFour(): NavigationParent {
-        return NavigationParent(
-                "Финансы",
-                makeParentFourChild(),
-                R.drawable.ic_user
-        )
-    }
-    
-    private fun makeParentFourChild(): List<NavigationChild> {
-        val child1 = NavigationChild("Баланс: 1000 Руб", 0)
-        val child2 = NavigationChild("Пополнить баланс", 0)
-        val child3 = NavigationChild("История платежей", 0)
-        val child4 =
-                NavigationChild("Оказанные услуги", R.drawable.ic_user)
-        val child5 = NavigationChild("Оказанные услуги (детально)", 0)
-        val child6 = NavigationChild("Рефальная программа", 0)
-        val child7 = NavigationChild("Cashback", 0)
-
-        return listOf(child1, child2, child3, child4, child5, child6, child7)
     }
 }
 
