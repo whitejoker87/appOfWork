56c8afdabdfa283ef9497d594b9c1125ff38cec5
NetylkinOV <gpcore@mail.ru>
2019-03-29 05:01:44 +0700

    typo

--- app/src/main/java/ru/jobni/jobni/fragments/menuleft/FragmentNavLeftOwnerAuthOff.kt
+++ app/src/main/java/ru/jobni/jobni/fragments/menuleft/FragmentNavLeftOwnerAuthOff.kt
@@ -67,14 +67,4 @@ class FragmentNavLeftOwnerAuthOff : Fragment() {
             }
         })
     }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        navAdapter.onSaveInstanceState(outState)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        navAdapter.onRestoreInstanceState(savedInstanceState)
-    }
 }
